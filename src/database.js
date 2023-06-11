01b2c3a38ef4f5ffe822cacd1517f0b7293a6f79
Wilson08 <wilson.guayanay@epn.edu.ec>
2023-06-11 18:38:33 -0500

    database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
+// npm install mongoose --save
+
 const mongoose = require('mongoose')
 
-const MONGODB_URI = 'mongodb://0.0.0.0:27017/portafolio'
+const MONGODB_URI = 'mongodb://0.0.0.0:27017/consumo_API'
 
 
 connection = async ()=>{
