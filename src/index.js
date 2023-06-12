e8c629e4bad680014dcb1e3f7392e59adcd9016d
Marlon <marlon.lalangui@epn.edu.ec>
2023-06-11 20:52:56 -0500

    cambio index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const app = require('./server.js')
-const connetion = require('./database.js')
 require('dotenv').config()
 
-connetion()
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`)
 })
